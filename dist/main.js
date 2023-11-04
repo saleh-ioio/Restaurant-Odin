8bdfc85ac9553d656c8980602837e9db1c9f54ec
Saleh Al-Saleh <saleh.alsaleh802@gmail.com>
2023-11-04 18:46:27 +0300

    setup project

---
+++ dist/main.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+
+console.log("running correctly");
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7QUFDQSxpQyIsInNvdXJjZXMiOlsid2VicGFjazovL3Jlc3RhdXJhbnQtb2Rpbi8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJcbmNvbnNvbGUubG9nKFwicnVubmluZyBjb3JyZWN0bHlcIik7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
